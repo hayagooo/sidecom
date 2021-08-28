df66ee0550bbf3bbfef579f69504b6641063f475
khulqu15 <khusnul.ninno15@gmail.com>
2021-08-28 11:09:04 +0700

    Relation User to Chat Model

---
+++ database/migrations/2021_08_28_034045_create_chats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id(); //BigInteger -> Unsigned
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->text('message');
+            $table->string('image');
+            $table->string('docs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
---
+++ resources/js/Pages/Admin/Chat/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{ label }}
+        <div v-if="chatData.length > 0">
+            <div v-for="item, index in chatData" :key="index">
+                <div class="border-2 border-indigo-600">
+                    <h3 class="text-xl">{{ item.message }}</h3>
+                    <p>from {{ item.user.name }}</p>
+                </div>
+            </div>
+        </div>
+        <div v-else>
+            <h2>Tidak ada chat terbaru</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['userData', 'chatData'],
+    data() {
+        return {
+            label: 'Chat Sidecom'
+        }
+    }
+}
+</script>
